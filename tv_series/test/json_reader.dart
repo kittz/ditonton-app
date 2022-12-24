8ddc66b12c35388b002b3165d5a0f54048d0a2db
kittz <suriemie@gmail.com>
2022-12-24 23:16:51 +0700

    Update json_reader.dart

--- tv_series/test/json_reader.dart
+++ tv_series/test/json_reader.dart
@@ -5,8 +5,8 @@ String readJson(String name) {
   if (dir.endsWith('/test')) {
     dir = dir.replaceAll('/test', '');
   }
-  if (dir.endsWith('tv')) {
+  if (dir.endsWith('tv_series')) {
     return File('$dir/test/$name').readAsStringSync();
   }
-  return File('$dir/tv/test/$name').readAsStringSync();
+  return File('$dir/tv_series/test/$name').readAsStringSync();
 }
